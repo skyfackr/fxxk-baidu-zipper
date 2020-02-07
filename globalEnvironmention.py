d7f9a2d44b17814561656b9b1bf98db8952cb020
Tony Jiang <44115200+skyfackr@users.noreply.github.com>
2020-02-07 22:33:06 +0800

    Update globalEnvironmention.py
    加入全局动态注册功能
    为了适应此功能对代码改了一通

--- globalEnvironmention.py
+++ globalEnvironmention.py
@@ -3,9 +3,9 @@
 @des:用于获取全局环境变量，对于不存在的变量，则会抛出异常
 '''
 import os,logging
-class globalEnv():
+class __GLOBALENV():
     '''
-    获取全局环境变量
+    （切勿直接使用,请使用globalEnv）获取全局环境变量
     '''
     #允许使用的变量名称
     __Member__=['OSSBucketName',
@@ -16,23 +16,45 @@ class globalEnv():
     'OSSMaxinumUpload',
     'OSSDownloadThread',
     'OSSUploadThread',
-    'Version']
+    'Version',
+    'support_mode',
+    'OSSMW']
     #直接输出的常量名，若不在此字典则读取系统环境变量，要求名称必须在__Member__中
     __envStaticMember={}
-    def isMenber(self,name):
+    @classmethod
+    def isMember(cls,name):
         '''
         测试环境变量是否存在
         '''
-        if name in self.__Member__:
+        if name in cls.__Member__:
             return True
         return False
 
+    @classmethod
     def __getattr__(self,name):
         logger=logging.getLogger()
         logger.info('get env:{}'.format(str(name)))
-        if not self.isMenber(name):
+        if not self.isMember(name):
             raise AttributeError('OS Env have no attribute called {}'.format(name))
         if name in self.__envStaticMember.keys():
             return self.__envStaticMember[name]
         return os.environ.get(name)
 
+    @staticmethod
+    def setStaticMember(name,value):
+        '''
+        设定直接输出变量，变量名必须注册于__Member__，否则抛出异常
+        '''
+        if not __GLOBALENV.isMember(name):
+            raise AttributeError('OS Env have no attribute called {}'.format(name))
+        __GLOBALENV.__envStaticMember[name]=value
+        return
+
+
+
+
+
+
+
+#我杀bug它全家
+globalEnv=__GLOBALENV()
