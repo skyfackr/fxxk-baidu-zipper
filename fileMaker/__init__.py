ebcf4be422c37f4af4f574720f80c731396e83b4
Tony Jiang <44115200+skyfackr@users.noreply.github.com>
2020-02-07 22:34:06 +0800

    Update __init__.py
    导入异常标识

--- fileMaker/__init__.py
+++ fileMaker/__init__.py
@@ -2,5 +2,5 @@
 @author:skyfackr
 @des:文件制作工具
 '''
-from .fileDecoder import decoder
-from .fileEncoder import file_decode as encoder
+from .fileDecoder import decoder,UnsupportError,VerificationError
+from .fileEncoder import file_encode as encoder
